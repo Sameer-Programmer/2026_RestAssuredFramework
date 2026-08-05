acc700e231eadb9930c2204372e3d8d851aaefe1
Sameer <sameeridea7@gmail.com>
2026-08-05 14:51:43 +0530

    demo2

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -8,16 +8,20 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class UserTests {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class UserTests {
+    private static final Logger logger = LogManager.getLogger(UserTests.class);
     Faker faker = new Faker();
     User userPayload = new User();
 
+
+
     @BeforeClass
-    public void setupData() {
+    public void setup() {
 
         System.out.println("Inside BeforeClass");
-
         userPayload.setId(faker.idNumber().hashCode());
         userPayload.setUsername(faker.name().username());
         userPayload.setFirstName(faker.name().firstName());
@@ -26,12 +30,14 @@ public class UserTests {
         userPayload.setPassword(faker.internet().password(5, 10));
         userPayload.setPhone(faker.phoneNumber().cellPhone());
         userPayload.setUserStatus(0);
-
         System.out.println("Generated Username: " + userPayload.getUsername());
+
     }
 
     @Test(priority = 1)
     public void testPostUser() {
+
+        logger.info("********Create user***************");
 
         System.out.println(userPayload.getUsername());
 
@@ -39,11 +45,14 @@ public class UserTests {
         response.then().log().all();
 
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("******** User Created***************");
     }
 
     @Test(priority = 2)
     public void testGetUserByName() {
 
+
+        logger.info("********Read user***************");
         System.out.println("Inside testGetUserByName()");
         System.out.println(userPayload.getUsername());
 
@@ -51,10 +60,14 @@ public class UserTests {
         response.then().log().all();
 
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("******** userRead info didplayed***************");
     }
 
      @Test(priority = 3)
     public void testUpdateUser() {
+
+         logger.info("********Updating  user***************");
+
 
          System.out.println("Inside testUpdateUserMethod");
          System.out.println(userPayload.getUsername());
@@ -67,11 +80,14 @@ public class UserTests {
 
         response.then().log().all();
         Assert.assertEquals(response.getStatusCode(), 200);
+
+         logger.info("********  User is Updated***************");
     }
 
      @Test(priority = 4)
     public void testDeleteUser() {
 
+         logger.info("********User is Deleting***************");
 
          System.out.println("Inside testDeleteUser");
          System.out.println(userPayload.getUsername());
@@ -79,5 +95,7 @@ public class UserTests {
 
         response.then().log().all();
         Assert.assertEquals(response.getStatusCode(), 200);
+
+         logger.info("********  Deleted User***************");
     }
 }
